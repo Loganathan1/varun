5131123704cf455f2fc6f19690df8a0f889476f9
Loganathan1 <30317720+Loganathan1@users.noreply.github.com>
2017-07-21 16:52:03 +0530

    Update and rename .gitignore to posneg.c

---
+++ .gitignore/posneg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num=5;
+  if(num>0)
+    { 
+     printf("positive");
+     }
+  elseif(num<0)
+       { 
+        printf("negative");
+       }
+     
+   else
+     {
+      printf("You are type Zero");
+      }
+}
