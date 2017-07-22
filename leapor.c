dad4748fc94468309ea2e8fb2b7625342f70f700
Loganathan1 <30317720+Loganathan1@users.noreply.github.com>
2017-07-22 17:24:33 +0530

    Create leapor.c

---
+++ leapor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+  { 
+   int year;
+   printf("Enter the year");
+   scanf("%d",&num);
+   if(num%4==0)
+     {
+      printf(" is leap year");
+      }
+   else
+     {
+       printf("is not a leap year");
+     }
+}
