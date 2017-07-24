8e90c69ff56cc0a0e56e1744f38345bcba0f58fa
Loganathan1 <30317720+Loganathan1@users.noreply.github.com>
2017-07-24 11:14:06 +0530

    Create fa.java

---
+++ fa.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class fa 
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a number:");
+int n=s.nextInt();
+int fact=1;
+for (int i=1;i<n;i++) 
+{
+  fact=fact*i;
+}
+System.out.println("Facto of the num is;"+fact);
+}
+}
