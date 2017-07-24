e59eae8ebdb26cff7658b208df4119b53feedb22
Loganathan1 <30317720+Loganathan1@users.noreply.github.com>
2017-07-24 11:17:06 +0530

    Create rev.c

---
+++ rev.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h> 
+int main() 
+  {
+    int i,j=0;
+   char st[50];
+   printf(" Enter the string:");
+   scanf("%s",st); 
+   i = 0;
+   j = strlen(st)-1; 
+   char temp;
+   while (i < j)
+     {
+      temp=st[i];
+      st[i]=st[j];
+      st[j]=temp;
+      i++;
+      j--;
+   } 
+   printf(" Reverse of string is:%s",str);
+   return(0);
+}
